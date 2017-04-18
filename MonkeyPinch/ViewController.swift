721cda65418eaa1f7b8651e34b09681cb966ef96
OsnielLopes <niellt@hotmail.com>
2017-04-18 13:42:31 -0300

    inicial

---
+++ MonkeyPinch/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  MonkeyPinch
+//
+//  Created by Osniel Lopes Teixeira on 17/04/17.
+//  Copyright © 2017 br.mackenzie.MackMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer){
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+        
+        if recognizer.state == UIGestureRecognizerState.ended{
+            let velocity = recognizer.velocity(in: self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            let slideFactor = 0.1 * slideMultiplier //Increase for more of a slide
+            
+            var finalPoint = CGPoint(x:recognizer.view!.center.x + (velocity.x * slideFactor),
+                                     y: recognizer.view!.center.y + (velocity.y * slideFactor))
+            
+            finalPoint.x = min(max(finalPoint.x, 0),self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            UIView.animate(withDuration: Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: { recognizer.view!.center = finalPoint }, completion: nil)
+        }
+    }
+    
+    @IBAction func handlePinche(recognizer: UIPinchGestureRecognizer){
+        if let view = recognizer.view{
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    @IBAction func handleRotate(recognizer : UIRotationGestureRecognizer){
+        if let view = recognizer.view {
+            view.transform = view.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
+
